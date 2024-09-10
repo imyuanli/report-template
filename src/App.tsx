9e516f06527b77dc2184bac6e47d5c08c2f99b5a
yuanli <2865437316@qq.com>
2024-09-10 16:09:48 +0800

    feat: 路由配置及页面基本布局

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,29 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from 'react';
+import {useRoutes} from 'react-router-dom'
+import routes from "./router";
+import './App.less'
+import {Button} from "antd";
 
-function App() {
-  const [count, setCount] = useState(0)
+const App: React.FC = () => {
+    const views = useRoutes(routes)
 
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+
+    return (
+        <div>
+            <header className="header">
+                <div>
+                    logo
+                </div>
+                <div>
+                    <Button>
+                        123
+                    </Button>
+                </div>
+            </header>
+            <main className="main">
+                {views}
+            </main>
+        </div>
+    )
 }
-
-export default App
+export default App;
---
+++ src/pages/index.tsx
@@ -0,0 +1,19 @@
+import "./index.less"
+
+const Index = () => {
+    return (
+        <div className="index">
+            <div className={"title"}>
+                廉政举报
+            </div>
+            <div>
+
+            </div>
+            <div>
+                详细内容
+            </div>
+        </div>
+    )
+}
+
+export default Index
